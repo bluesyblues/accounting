6c3b8bb95e2a3f31e9dc3871bcbd0aa1a951c14d
jwbaek <jwbaek@nota.ai>
2022-01-27 22:14:08 +0900

    venv and db_connection

---
+++ src/db_connection.py
@@ -0,0 +1,87 @@
+import fdb
+
+HOST = "127.0.0.1"
+USER = "sysdba"
+PASSWORD = "masterkey"
+MAPPING_TABLE = {
+    "영유아부헌금" : "영유아부",
+    "유초등부헌금" : "유초등부",
+    "중고등부헌금" : "중고등부",
+    "청년부헌금" : "청년부",
+    "주일헌금" : "주일",
+    "감사헌금" : "감사",
+    "십일조" : "십일조",
+    "헌신예배헌금": "헌신예배",
+    "건축헌금":"건축",
+    "구제헌금":"구제",
+    "선교헌금":"선교",
+    "장학헌금":"장학",
+    "목장헌금":"목장",
+    "지정헌금":"지정",
+    "특새헌금":"특새",
+    "새벽기도회":"새벽기도회",
+    "겟세마네":"겟세마네",
+    "수요예배헌금":"수요예배",
+    "심방헌금":"심방",
+    "부활절":"부활절",
+    "추수감사절":"추수감사절",
+    "신년예배(송구영신)":"송구영신예배",
+    "신년감사":"신년감사",
+    "재직세미나":"재직세미나",
+    "성미헌금":"성미",
+    "구제헌금(지정)":"구제헌금(지정)",
+    "선교헌금(지정)":"선교헌금(지정)",
+    "금요예배헌금":"금요예배헌금",
+    "성탄절":"성탄절"
+}
+
+def connect_to_db(database):
+    con = fdb.connect(
+        host=HOST, 
+        database=database, 
+        user=USER, 
+        password=PASSWORD,
+        charset='UTF8'
+    )
+
+    return con
+
+def send_query(connection, query_str):
+    cur = connection.cursor()
+    cur.execute(query_str)
+    
+    return cur.fetchall()
+
+
+def get_raw_data(accounting_date):
+    query_str = f"""
+        SELECT 
+            CD.CD_TEXT,
+            CM.CM_NAME,
+            FI_AMOUNT, 
+            CASE WHEN FI_COMM_GIFTER = '통장' THEN '통장'
+            ELSE FI_COMM_GIFTER END AS FI_COMM_GIFTER, 
+            FI_IN_DATE, 
+            FI_IMPORT_TYPE, 
+            FI_DEBTOR_CM_SEQ,
+            FI_ACCOUNT
+        FROM FINANCE_IMPORT AS FI
+        JOIN CHRIST_MEMBER AS CM
+        ON FI.FI_DEBTOR_CM_SEQ = CM.CM_SEQ AND FI_IN_DATE='{accounting_date}'
+        JOIN CHRIST_CODE AS CD
+        ON FI.FI_ACCOUNT = CD.CD_SEQ
+        ORDER BY CD.CD_TEXT
+    """
+    DATABASE=r'C:\Users\blues\workspace\accounting\samples\sample_DB\FBUCMSDATA.FDB'
+    con = connect_to_db(DATABASE)
+    query_result = send_query(con, query_str)
+    for i in range(0, len(query_result)):
+        query_result[i] = list(query_result[i])
+        simple_type = MAPPING_TABLE.get(query_result[i][0])
+        if simple_type :
+            query_result[i][0] = simple_type
+        else:
+            query_result[i][0] = "NULL"
+
+    return query_result
+
